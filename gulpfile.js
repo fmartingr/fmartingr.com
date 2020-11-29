6983cb1c3a53e529ed08759b4a37c0c5de26d74a
Felipe Martin <me@fmartingr.com>
2020-11-29 21:32:54 +0100

    Removed Javascript from the site

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concatCss = require('gulp-concat-css');
-var concatJs = require('gulp-concat');
 var merge = require('merge-stream');
 var minifyCss = require('gulp-minify-css');
-var minify = require('gulp-minify');
 
 gulp.task('sass', function() {
     var sassStream = gulp.src('./static/sass/style.sass')
@@ -17,19 +15,6 @@ gulp.task('sass', function() {
         .pipe(minifyCss())
         .pipe(gulp.dest('./assets/static/css'));
 });
-
-gulp.task('js', function() {
-    var jsStream = gulp.src([
-        './static/js/main.js'
-    ]);
-    return jsStream
-        .pipe(concatJs('app.js'))
-        .pipe(minify({
-            exclude: [],
-            ignoreFiles: []
-        }))
-        .pipe(gulp.dest('./assets/static/js'))
-})
 
 gulp.task('sass:watch', function() {
     gulp.watch('./static/sass/*', gulp.parallel(['sass']));
--- static/js/main.js
+++
@@ -1,5 +0,0 @@
-// Fix for lektor highlight plugin
-var elements = document.querySelectorAll('div.highlight');
-for (var i = 0; i < elements.length; i++) {
-  elements[i].classList.add('hll');
-}
